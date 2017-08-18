97d36bee2d816eb429ae94719a1cbd761a3bdc7c
alvin-luhao <649233677@qq.com>
2017-08-18 00:57:37 -0400

    kernel

--- linux-xlnx/include/generated/compile.h
+++ linux-xlnx/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Fri Aug 18 00:40:32 EDT 2017"
+#define UTS_VERSION "#4 SMP PREEMPT Fri Aug 18 00:57:11 EDT 2017"
 #define LINUX_COMPILE_BY "alvin"
 #define LINUX_COMPILE_HOST "xlx"
 #define LINUX_COMPILER "gcc version 5.2.1 20151005 (Linaro GCC 5.2-2015.11-2) "
