0f4ec56a31950836d547c33ba7a50337790f5308
alvin-luhao <649233677@qq.com>
2017-08-18 00:51:20 -0400

    kernel

---
+++ linux-xlnx/arch/arm/boot/compressed/.vmlinux.lds.cmd
@@ -0,0 +1,10 @@
+cmd_arch/arm/boot/compressed/vmlinux.lds := arm-linux-gnueabihf-gcc -E -Wp,-MD,arch/arm/boot/compressed/.vmlinux.lds.d  -nostdinc -isystem /home/alvin/petalinux2016.4/tools/linux-i386/gcc-arm-linux-gnueabi/bin/../lib/gcc/arm-linux-gnueabihf/5.2.1/include -I./arch/arm/include -I./arch/arm/include/generated/uapi -I./arch/arm/include/generated  -I./include -I./arch/arm/include/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian     -DTEXT_START="0" -DBSS_START="ALIGN(8)" -P -C -Uarm -D__ASSEMBLY__ -DLINKER_SCRIPT -o arch/arm/boot/compressed/vmlinux.lds arch/arm/boot/compressed/vmlinux.lds.S
+
+source_arch/arm/boot/compressed/vmlinux.lds := arch/arm/boot/compressed/vmlinux.lds.S
+
+deps_arch/arm/boot/compressed/vmlinux.lds := \
+    $(wildcard include/config/cpu/endian/be8.h) \
+
+arch/arm/boot/compressed/vmlinux.lds: $(deps_arch/arm/boot/compressed/vmlinux.lds)
+
+$(deps_arch/arm/boot/compressed/vmlinux.lds):
